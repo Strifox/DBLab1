4dad4e04a68de3c9f1c7f24e9aa06644e723a924
Johan Svensson <johan.svensson93@hotmail.se>
2017-11-21 11:18:05 +0100

    added new sql connection

--- DBLab1/MainWindow.xaml.cs
+++ DBLab1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,8 +21,11 @@ namespace DBLab1
     /// </summary>
     public partial class MainWindow : Window
     {
+        SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog= DBLab1;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
         public MainWindow()
         {
+
             InitializeComponent();
         }
     }
