ca5dfd1431ee2032cba0ccbc80dc56678f1a059c
Johan Svensson <johan.svensson93@hotmail.se>
2017-11-23 10:26:27 +0100

    Added ADO.NET to Mainwindow

--- DBLab1/MainWindow.xaml.cs
+++ DBLab1/MainWindow.xaml.cs
@@ -21,12 +21,64 @@ namespace DBLab1
     /// </summary>
     public partial class MainWindow : Window
     {
-        SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog= DBLab1;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        private SqlConnection sqlUpdate = new SqlConnection();
 
+        private string dbConnectionString =
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DB Lab1;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"
+            ;
         public MainWindow()
         {
-
             InitializeComponent();
+        }
+
+        private void ShowDataBase()
+        {
+            SqlConnection con =new SqlConnection(dbConnectionString);
+
+            try
+            {
+                con.Open();
+                string query = "SELECT * FROM Author";
+                SqlCommand cmd = new SqlCommand(query, con);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string row = "";
+
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        row += reader[i] + " ";
+                    }
+                    listboxAuthor.Items.Add(row);
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+        private void txtBoxAuthorName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //if (string.IsNullOrWhiteSpace(txtBoxAuthorName.Text))
+            //    MessageBox.Show("Insert a name");
+            //else
+            //{
+            //    btnAdd.IsEnabled = true;
+
+            //}
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            ShowDataBase();
         }
     }
 }
